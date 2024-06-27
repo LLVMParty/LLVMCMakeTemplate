cde634b36d426a7f9261934acde5df32ff3c0a5f
Duncan Ogilvie <mr.exodia.tpodt@gmail.com>
2024-06-27 16:57:31 +0200

    Update FindLLVM-Wrapper

--- src/example.cpp
+++ src/example.cpp
@@ -1,75 +1,47 @@
 #include <cstdlib>
+#include <cstdio>
 
-#include <llvm/Support/raw_ostream.h>
-#include <llvm/Support/FormattedStream.h>
-#include <llvm/Support/SourceMgr.h>
-#include <llvm/Support/ToolOutputFile.h>
-#include <llvm/Support/FileSystem.h>
-#include <llvm/IRReader/IRReader.h>
-#include <llvm/IR/IRBuilder.h>
-#include <llvm/IR/Instructions.h>
-#include <llvm/Bitcode/BitcodeWriter.h>
+#include "utility.hpp"
 
 using namespace llvm;
 
-static void ProcessModule(Module& M)
-{
-    // TODO: do something meaningful here
-    for (Function& F : M.functions())
-    {
-        for (BasicBlock& BB : F)
-        {
-            for (Instruction& I : BB)
-            {
-                if (auto Load = dyn_cast<LoadInst>(&I))
-                {
-                }
-            }
+static void ProcessModule(Module &M) {
+  // TODO: do something meaningful here
+  for (Function &F : M.functions()) {
+    for (BasicBlock &BB : F) {
+      for (Instruction &I : BB) {
+        if (auto Load = dyn_cast<LoadInst>(&I)) {
         }
+      }
     }
+  }
 }
 
-int main(int argc, char** argv)
-{
-    if (argc < 3)
-    {
-        puts("Usage: example in.bc out.bc");
-        return EXIT_FAILURE;
-    }
-    auto inFile = argv[1];
-    auto outFile = argv[2];
+int main(int argc, char **argv) {
+  // Parse arguments
+  if (argc < 2) {
+    puts("Usage: example in.bc [out.bc]");
+    return EXIT_FAILURE;
+  }
+  auto inFile = argv[1];
+  auto outFile = argc > 2 ? argv[2] : nullptr;
 
-    // Load module
-    LLVMContext C;
-    SMDiagnostic Err;
-    auto M = parseIRFile(inFile, Err, C);
-    if (!M)
-    {
-        outs() << "Failed to parse IR: " << Err.getMessage() << "\n";
-        return EXIT_FAILURE;
-    }
+  // Load module
+  LLVMContext C;
+  auto M = LoadModule(C, inFile);
 
-    // Process module
-    try
-    {
-        ProcessModule(*M);
-    }
-    catch (const std::exception& x)
-    {
-        outs() << x.what() << "\n";
-        return EXIT_FAILURE;
-    }
+  // Process module
+  try {
+    ProcessModule(*M);
+  } catch (const std::exception &x) {
+    outs() << x.what() << "\n";
+    return EXIT_FAILURE;
+  }
 
-    // Write module
-    std::error_code EC;
-    ToolOutputFile Out(outFile, EC, sys::fs::OF_None);
-    WriteBitcodeToFile(*M, Out.os(), true);
-    if (EC)
-    {
-        outs() << "Failed to write IR: " << EC.message() << "\n";
-        return EXIT_FAILURE;
-    }
-    Out.keep();
+  // Save module
+  if (outFile != nullptr) {
+    SaveModule(M.get(), outFile);
+  }
 
-    return EXIT_SUCCESS;
+  return EXIT_SUCCESS;
 }
